2d3f4677ce1787a0f90f7e9071f58a3a09ce6347
Maddaloni Michele <M.Maddaloni@almaviva.it>
2019-01-21 16:09:39 +0100

    michele

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,7 +8,8 @@ import { Observable } from 'rxjs';
 
 @Component({
     selector: 'home-component',
-    templateUrl: 'home.component.html'
+    templateUrl: 'home.component.html',
+    styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
     currentUser: User;
